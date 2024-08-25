6204e920f87f688b9e1fcb421a25dd1977718077
Soul <153772828+Soulful0@users.noreply.github.com>
2024-08-25 11:15:24 -0700

    Removed views folder and put html files in the root of public

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Set static folder
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// Serve HTML files from the 'views' directory
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'public', 'about.html')));
 app.get('/experience', (req, res) => res.sendFile(path.join(__dirname, 'public', 'experience.html')));
