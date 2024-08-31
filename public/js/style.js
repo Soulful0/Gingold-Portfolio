5b73e9a90a5d11f7c7ffd55d76f8d02c4d6473e6
Soul <153772828+Soulful0@users.noreply.github.com>
2024-08-30 22:35:05 -0700

    temp

--- public/js/style.js
+++ public/js/style.js
@@ -3,10 +3,54 @@
         const scrollable = document.querySelector('.scrollable');
         new SmoothScroll(scrollable)
         
-        // Add the 'shrink' class after a brief delay when the page loads
-        window.onload = function() {
-            setTimeout(function() {
-                document.querySelector('.headshot').classList.add('headshot-shrink');
-            }, 50); // Delay of 100 milliseconds (can be adjusted)
-        };
+// Add the 'shrink' class after a brief delay when the page loads
+    window.onload = function() {
+    setTimeout(function() {
+        document.querySelector('.headshot').classList.add('headshot-shrink');
+       }, 50); // Delay of 100 milliseconds (can be adjusted)
+    };
 
+
+        // Scroll Effect
+
+let line1 = document.querySelector('.line-1');
+// let line2 = document.querySelector('.line-2');
+
+window.onscroll = () => {
+    let pos = window.scrollY;
+
+    // Calculate the opacity based on scroll position (adjust the divisor for faster/slower fading)
+    let opacityValue = Math.min(1, pos / 1000); // Adjust 500 to control how quickly it fades
+
+    // Move line1 to the right and decrease opacity as you scroll down
+    line1.style.transform = `translateX(${pos}px)`;
+    line1.style.opacity = 1 - opacityValue;
+
+    // Move line2 to the right and decrease opacity as you scroll down
+    // line2.style.transform = `translateX(${-pos}px)`;
+    // line2.style.opacity = 1 - opacityValue;
+};
+
+// Initial fade in effect
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Fade-in effect for text elements
+    const leftElements = [
+      document.getElementById('fadeInEffect-left-1'),
+    //   document.getElementById('fadeInEffect-left-2')
+    ];
+    // const rightElements = [
+    //   document.getElementById('fadeInEffect-right-1'),
+    //   document.getElementById('fadeInEffect-right-2')
+    // ];
+  
+    leftElements.forEach(element => element.classList.add('fade-in'));
+    // rightElements.forEach(element => element.classList.add('fade-in'));
+});
+
+// Image size effect
+
+window.addEventListener('load', function() {
+    const img = document.querySelector('#header-img-style');
+    img.style.width = '34vw'; // Ensure width is set on full page load
+});
